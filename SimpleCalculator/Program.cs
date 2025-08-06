442a5bf9394965c4b41bae319f7d73b0a23a66a0
eb.babaei <eb.babaei@gmail.com>
2025-08-06 21:13:06 +0330

    افزودن پیام خوشامدگویی و توضیحات خروج

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -4,3 +4,12 @@ Console.WriteLine("        Simple Calculator        ");
 Console.WriteLine("=================================");
 Console.WriteLine("A basic console application for simple math operations.");
 
+Console.WriteLine("Developed by Bugeto.net");
+Console.WriteLine();     
+
+Console.WriteLine("This is the initial version. More features coming soon!");
+Console.WriteLine();
+
+ 
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
