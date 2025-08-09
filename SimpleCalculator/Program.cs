2b0d2b785411f456760a2f274a63ed110f91ace9
eb.babaei <eb.babaei@gmail.com>
2025-08-09 15:06:37 +0330

    تغییر متن خروجی به Sum

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -23,7 +23,7 @@ int num2 = int.Parse(Console.ReadLine());
 
 int result = calc.Sum(num1, num2);
 
-Console.WriteLine($"Sum result: {result}");
+Console.WriteLine($"Sum: {result}");
 
 
 Console.WriteLine("Press any key to exit...");
